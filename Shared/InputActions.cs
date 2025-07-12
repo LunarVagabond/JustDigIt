cd76e143bb89f3045f5bb7869d1b1ae243f5fd12
Lunar Vagabond <lvagabond@dcorps.dev>
2025-07-12 15:37:47 -0400

    [day 1/10] - Better layer names and input actions script

---
+++ Shared/InputActions.cs
@@ -0,0 +1,23 @@
+namespace CustomInputActions
+{
+
+  /* Import this as: 
+      using IA = CustomInputActions.InputActions;
+  */
+  public class InputActions
+  {
+
+
+    #region  Movement
+    public const string MOVE_LEFT = "move_left";
+    public const string MOVE_RIGHT = "move_right";
+    public const string JUMP = "jump";
+    public const string INTERACT = "interact";
+
+    #endregion
+
+    #region UI
+    public const string Pause = "pause";
+    #endregion
+  }
+}
