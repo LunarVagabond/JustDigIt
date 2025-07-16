061186b50e69e80af0a9c7356a00db7bf373596e
Lunar Vagabond <lvagabond@dcorps.dev>
2025-07-16 13:20:56 -0400

    [day 5/10] - Able to add items to the craftin recipes now

--- UserInterface/CraftingMenu/CraftingMenu.cs
+++ UserInterface/CraftingMenu/CraftingMenu.cs
@@ -1,11 +1,19 @@
 using Godot;
-using IA = CustomInputActions.InputActions;
-
 public partial class CraftingMenu : Control
 {
+  [Export] private VBoxContainer recepiesContainer;
+  [Export] private PackedScene recepieScene;
 
-  public override void _Ready()
+
+  // TODO: The arg passed in probably should be some other type like "Blueprint" or "BP" or something
+  // Based on the blueprint pickup whne it happens we can add it into recipes here 
+  public void AddRecpie(Pickup pickup)
   {
+    Recipe r = recepieScene.Instantiate<Recipe>();
+    // NOTE: IDK why but we need to add the item to the scene THEN update the texture of it
+    // I think it's because the texture is null untill the _Ready() runs for the recepieScene
+    recepiesContainer.AddChild(r);
+    r.Icon.Texture = pickup.Item.icon;
   }
 
   public void ToggleVisable()
--- UserInterface/CraftingMenu/Recipe.cs
+++ UserInterface/CraftingMenu/Recipe.cs
@@ -1,11 +1,18 @@
 using Godot;
-using System;
-using System.Runtime.Serialization;
 
 public partial class Recipe : PanelContainer
 {
+  public TextureRect Icon;
+
+  public override void _Ready()
+  {
+    Icon = GetNode<TextureRect>("HBoxContainer/Icon");
+    GD.Print(Icon);
+  }
+
   private void OnSelected()
   {
     GD.Print("This button should trigger the RightSidePanel in CraftinMenu scene to populate with _stuff_");
   }
 }
+
